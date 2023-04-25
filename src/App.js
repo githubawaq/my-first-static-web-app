97ba40f42c4dbc1b588cabdb8662d3d31c7f5eaf
githubawaq <126294937+githubawaq@users.noreply.github.com>
2023-04-25 15:32:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Home from "./components/Home/index";
 
 function App() {
   const value = 'acá van los módulos. Awaq Campus Internship! Aunque no parezca mucho si estás viendo esta línea quiere decir que descubrimos como deployar en Azure.';
-  return <div>--> {value}</div>;
+  return <div>-->   
+    <Home />
+  </div>;
 }
 
 export default App;

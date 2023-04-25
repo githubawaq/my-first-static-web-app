b36478d5f5cf5f5f5ee6d857652743968b15c850
githubawaq <126294937+githubawaq@users.noreply.github.com>
2023-04-25 16:08:13 -0300

    Update index.jsx

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Headboard from "./Headboard";
 
 const Home = () => {
   
   return (
     <div>    
     <p>prueba</p>
+    <Headboard />      
     </div>
   );
 };

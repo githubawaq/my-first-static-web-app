864866a4aab07f35890def31b34f851c72fe3cba
githubawaq <126294937+githubawaq@users.noreply.github.com>
2023-05-03 15:28:28 -0300

    Update index.jsx

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import Headboard from "./Headboard";
 import Board from "./Board";
 import Programs from "./Programs";
+import Housing from "./Housing";
+import Destination from "./Destination";
 import NavBar  from "../NavBar/NavBar";
+import WhyTravel from "./WhyTravel"
 import Footer  from "../Footer/Footer";
 
 const Home = () => {
   
   return (
     <div>      
-    <NavBar /> 
-    <Headboard />      
-    <Board />
-    <Footer />   
+      <NavBar /> 
+      <Headboard />          
+      <Programs />
+      <Housing />
+      <Destination />
+      <WhyTravel />
+      <Board />
+      <Footer />   
     </div>
   );
 };
